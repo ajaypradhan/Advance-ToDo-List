fd0ca50688226700833dd145f12b0af4009941b8
Ajay Pradhan <pradhanajay14@gmail.com>
2021-07-25 00:33:25 +0530

    add item to todo list

--- app.js
+++ app.js
@@ -6,39 +6,30 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let items = [];
+
 app.get('/', function (req, res) {
     let today = new Date();
-    let currentDay = today.getDay();
-    let day = '';
 
-    switch (currentDay) {
-        case 0:
-            day = 'Sunday';
-            break;
-        case 1:
-            day = 'Monday';
-            break;
-        case 2:
-            day = 'Tuesday';
-            break;
-        case 3:
-            day = 'Wednesday';
-            break;
-        case 4:
-            day = 'Thursday';
-            break;
-        case 5:
-            day = 'Friday';
-            break;
-        case 6:
-            day = 'Saturday';
-            break;
-        default:
-            console.log('Error: Current day is not valid');
-    }
-    res.render('list', { kindOfDay: day });
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+    };
+
+    let day = today.toLocaleDateString('en-US', options);
+
+    res.render('list', {
+        kindOfDay: day,
+        newListItems: items,
+    });
 });
 
+app.post('/', function (req, res) {
+    let item = req.body.newItem;
+    items.push(item);
+    res.redirect('/');
+});
 app.listen(process.env.PORT || 3000, function () {
     console.log('Server is running');
 });
