27a5b067b8de0edc52a7bd46652d46e3bae06439
Ajay Pradhan <pradhanajay14@gmail.com>
2021-07-24 21:29:46 +0530

    day in ejs

--- app.js
+++ app.js
@@ -11,12 +11,30 @@ app.get('/', function (req, res) {
     let currentDay = today.getDay();
     let day = '';
 
-    if (currentDay === 6 || currentDay === 0) {
-        day = 'weekend';
-        // res.render('list', { kindOfDay: day });
-    } else {
-        day = 'weekday';
-        // res.render('list', { kindOfDay: day });
+    switch (currentDay) {
+        case 0:
+            day = 'Sunday';
+            break;
+        case 1:
+            day = 'Monday';
+            break;
+        case 2:
+            day = 'Tuesday';
+            break;
+        case 3:
+            day = 'Wednesday';
+            break;
+        case 4:
+            day = 'Thursday';
+            break;
+        case 5:
+            day = 'Friday';
+            break;
+        case 6:
+            day = 'Saturday';
+            break;
+        default:
+            console.log('Error: Current day is not valid');
     }
     res.render('list', { kindOfDay: day });
 });
